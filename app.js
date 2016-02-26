701fe204681f6bc569157210756f142c3ec505a3
sjf6598 <sjf6598@gmail.com>
2016-02-26 11:58:47 -0500

    fav icon

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 var loginDao =new LoginDao();
 var login = new Login(loginDao);
